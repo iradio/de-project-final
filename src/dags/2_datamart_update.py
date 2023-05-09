6c9b9da10f0c0ab90d4c5d4568f210b38282f96e
tim.aleinikov <tim.aleinikov@gmail.com>
2023-05-09 14:46:27 +0600

    cdm: global_metrics calculation

---
+++ src/dags/2_datamart_update.py
@@ -0,0 +1,111 @@
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.decorators import dag
+from airflow.models import Variable
+
+from datetime import datetime, timedelta
+# import boto3
+
+import vertica_python
+
+# import the logging module
+import logging
+
+# get the airflow.task logger
+logger = logging.getLogger("airflow.task")
+
+
+# Don't forget to set up Connection and Variable in Airflow UI
+vertica_connection = Variable.get("vertica_connection", deserialize_json=True)
+stg_schema_name = 'TIM_ALEINIKOV_YANDEX_RU__STAGING'
+cdm_schema_name = 'TIM_ALEINIKOV_YANDEX_RU__DWH'
+cdm_table_name = 'global_metrics'
+
+
+
+def calculate_global_metrics(connection, table_name, **context):
+    # достаем дату запуска DAG и сдвигаем на -1 день для извлечения данных за "вчера"
+    execution_date = (context['logical_date'] - timedelta(days=1) ).strftime('%Y-%m-%d') 
+
+    # SQL string definition
+    delete_sql = f"""
+        delete from {cdm_schema_name}.{table_name}
+        where date_update = to_date('{execution_date}','yyyy-mm-dd')    
+    """
+
+    insert_sql = f"""
+        insert into {cdm_schema_name}.{table_name}
+        with chargeback as (
+            select operation_id 
+            from {stg_schema_name}.transactions t
+            where status = 'chargeback'
+        ),
+        r as (
+            select 
+            to_char(t.transaction_dt,'yyyy-mm-dd') as transaction_day
+            ,t.currency_code as  currency_from
+            , t.amount as amount 
+            , COALESCE(c.currency_with_div, 1) as usd_currency_div
+            , account_number_from as account_make_transaction
+            from {stg_schema_name}.transactions t
+            left join chargeback cb using (operation_id)
+            left join {stg_schema_name}.currencies c 
+                on to_char(c.date_update,'yyyy-mm-dd') = to_char(t.transaction_dt,'yyyy-mm-dd')
+                                and c.currency_code_with = 420 
+                                and t.currency_code  = c.currency_code
+            where cb.operation_id is null
+        		and transaction_day = '{execution_date}'
+                and t.account_number_from != -1
+                and t.status = 'done'
+            order by t.operation_id
+        ) 
+        select 
+            to_date(r.transaction_day,'yyyy-mm-dd') as date_update
+            , r.currency_from::int as currency_from
+            , sum(r.amount * r.usd_currency_div)::numeric(16,2) as amount_total 
+            , count(*)::int as cnt_transactions
+            , count(distinct account_make_transaction)::int as cnt_accounts_make_transactions 
+            , cnt_transactions / cnt_accounts_make_transactions::numeric(16,2) as avg_transactions_per_account
+        from r
+        group by r.transaction_day, r.currency_from
+        order by r.transaction_day, r.currency_from
+    """
+
+    conn = vertica_python.connect(**connection)
+    cursor = conn.cursor()
+
+    logger.info(f"Delete values from {cdm_schema_name}.{table_name} by date {execution_date}")
+    cursor.execute(delete_sql)
+
+    logger.info(f"Insert values into: {cdm_schema_name}.{table_name} fro {execution_date}")
+    cursor.execute(insert_sql)
+
+    conn.commit()
+    
+    cursor.close()
+    conn.close()
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 12, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=30)
+}
+
+dag = DAG('cdm_global_metrics', default_args=default_args, schedule_interval='@daily')
+
+calculate_global_metrics_task = PythonOperator(
+    task_id='calculate_global_metrics_task',
+    python_callable=calculate_global_metrics,
+    op_kwargs={"connection": vertica_connection,
+               "table_name": cdm_table_name
+               },
+    provide_context=True,
+    dag=dag
+)
+
+# Set up task order
+calculate_global_metrics_task
--- src/sql/cdm_ddl.sql
+++ src/sql/cdm_ddl.sql
@@ -1,11 +1,14 @@
-create table TIM_ALEINIKOV_YANDEX_RU__DWH.global_metrics
+drop table if exists TIM_ALEINIKOV_YANDEX_RU__DWH.global_metrics
+
+create table if not exists TIM_ALEINIKOV_YANDEX_RU__DWH.global_metrics
 (
-	date_update TIMESTAMP(0), -- дата расчёта,
-	currency_from INT, -- код валюты транзакции;
-	amount_total NUMERIC(5, 3) NULL, -- общая сумма транзакций по валюте в долларах;
-	cnt_transactions NUMERIC(5, 3) NULL, -- общий объём транзакций по валюте;
-	avg_transactions_per_account NUMERIC(5, 3) NULL, -- средний объём транзакций с аккаунта;
-	cnt_accounts_make_transactions INT -- количество уникальных аккаунтов с совершёнными транзакциями по валюте.
+	date_update TIMESTAMP(0) NOT NULL, -- дата расчёта,
+	currency_from INT NOT NULL, -- код валюты транзакции;
+	amount_total NUMERIC(16, 2) NOT NULL, -- общая сумма транзакций по валюте в долларах;
+	cnt_transactions INT NOT NULL, -- общий объём транзакций по валюте;
+	avg_transactions_per_account NUMERIC(16, 2) NULL, -- средний объём транзакций с аккаунта;
+	cnt_accounts_make_transactions INT, -- количество уникальных аккаунтов с совершёнными транзакциями по валюте.
+	CONSTRAINT global_metrics_pk PRIMARY KEY (date_update, currency_from) ENABLED
 )
 order by date_update
 segmented by hash(date_update, currency_from) all nodes
